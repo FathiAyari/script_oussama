873fad20cfb057db09b2e6a0de87c0953640485c
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2024-04-04 20:36:17 +0200

    add index of every line

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 from watchdog.observers import Observer
@@ -24,14 +23,16 @@ def treat_data(file_path):
     valuesX1 = []
     valuesYu = []
     valuesYo = []
-
+    flag_write = False
+    done_getting_max = False
     with open(file_path, 'r') as fileToTreat:
         # Read all lines from the file
         lines = fileToTreat.readlines()
 
         x = ""
         y = ""
-        # Display each line with its line number
+        line_index = 0
+
         for i, line in enumerate(lines, start=1):
             if i == 5:
                 date = line.strip().split(":")[1]
@@ -44,6 +45,7 @@ def treat_data(file_path):
                 else:
                     valuesY.extend([int(val) for val in line.strip().split()])
 
+                    flag_write = True
             if i == 18:
                 valuesX1.extend([str(val) for val in line.strip().split()])
             if i == 19:
@@ -54,73 +56,90 @@ def treat_data(file_path):
             if line.strip() == "Messreihen (X,Y)":
                 flag = True
 
+            if flag_write:
+
+                try:
+                    connection = pymysql.connect(**connection_config)
+                    print("Connected to MySQL database")
+                    cursor = connection.cursor()
+
+                    create_table_query = """
+                        CREATE TABLE IF NOT EXISTS your_table (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            line_index int(10),
+                            x int(10),
+                            y int(10),
+                            werkauftrag varchar(255),
+                            datum varchar(255)
+                        )
+                    """
+                    get_max_index_query = """select  max(line_index) from your_table"""
+                    cursor.execute(get_max_index_query)
+                    max_index = cursor.fetchone()[0]
+
+                    if max_index is None:
+                        line_index += 1
+                    elif not done_getting_max:
+                        line_index = max_index + 1
+                        done_getting_max = True
+                    else:
+                        line_index += 1
+
+                    # Execute the SQL query
+                    cursor.execute(create_table_query)
+                    print("Table created successfully or already exists")
+
+                    data_to_insert = [(line_index, x, y, werkauftrag, date) for x, y in zip(valuesX, valuesY)]
+                    # SQL query to insert data1.txt into the table
+                    insert_query = """
+                        INSERT INTO your_table (line_index,x, y,werkauftrag, datum)
+                        VALUES (%s, %s, %s,%s,%s)
+                    """
+                    # Execute the SQL query for each data1.txt row
+                    cursor.executemany(insert_query, data_to_insert)
+                    connection.commit()
+                    print("Data inserted successfully to your_table")
+
+                    ###############################################################################
+
+                    create_table2_query = """
+                        CREATE TABLE IF NOT EXISTS your_table2 (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            x varchar(255),
+                            yu varchar(255),
+                            yo varchar(255),
+                            werkauftrag varchar(255),
+                            datum varchar(255)
+                        )
+                    """
+
+                    # Execute the SQL query
+                    cursor.execute(create_table2_query)
+                    print("Table created successfully or already exists")
+
+                    data_to_insert = [(x, yo, yu, werkauftrag, date) for x, yu, yo in zip(valuesX1, valuesYu, valuesYo)]
+                    # SQL query to insert data1.txt into the table
+                    insert_query = """
+                        INSERT INTO your_table2 (x, yu,yo,werkauftrag, datum)
+                        VALUES (%s, %s,%s, %s,%s)
+                    """
+                    # Execute the SQL query for each data1.txt row
+                    cursor.executemany(insert_query, data_to_insert)
+                    connection.commit()
+                    print("Data inserted successfully to your_table2")
+                except pymysql.Error as e:
+                    print(f"Error connecting to MySQL database: {e}")
+                finally:
+                    # Close cursor and connection
+                    if 'cursor' in locals():
+                        cursor.close()
+                    if 'connection' in locals() and connection.open:
+                        connection.close()
+                valuesX = []
+                valuesY = []
+                flag_write = False
+
     # Database connection details
-
-    try:
-        connection = pymysql.connect(**connection_config)
-        print("Connected to MySQL database")
-        cursor = connection.cursor()
-
-        create_table_query = """
-            CREATE TABLE IF NOT EXISTS your_table (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                x int(10),
-                y int(10),
-                werkauftrag varchar(255),
-                datum varchar(255)
-            )
-        """
-
-        # Execute the SQL query
-        cursor.execute(create_table_query)
-        print("Table created successfully or already exists")
-
-        data_to_insert = [(x, y, werkauftrag, date) for x, y in zip(valuesX, valuesY)]
-        # SQL query to insert data1.txt into the table
-        insert_query = """
-            INSERT INTO your_table (x, y,werkauftrag, datum)
-            VALUES (%s, %s, %s,%s)
-        """
-        # Execute the SQL query for each data1.txt row
-        cursor.executemany(insert_query, data_to_insert)
-        connection.commit()
-        print("Data inserted successfully to your_table")
-
-        ###############################################################################
-
-        create_table2_query = """
-            CREATE TABLE IF NOT EXISTS your_table2 (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                x varchar(255),
-                yu varchar(255),
-                yo varchar(255),
-                werkauftrag varchar(255),
-                datum varchar(255)
-            )
-        """
-
-        # Execute the SQL query
-        cursor.execute(create_table2_query)
-        print("Table created successfully or already exists")
-
-        data_to_insert = [(x, yo, yu, werkauftrag, date) for x, yu, yo in zip(valuesX1, valuesYu, valuesYo)]
-        # SQL query to insert data1.txt into the table
-        insert_query = """
-            INSERT INTO your_table2 (x, yu,yo,werkauftrag, datum)
-            VALUES (%s, %s,%s, %s,%s)
-        """
-        # Execute the SQL query for each data1.txt row
-        cursor.executemany(insert_query, data_to_insert)
-        connection.commit()
-        print("Data inserted successfully to your_table2")
-    except pymysql.Error as e:
-        print(f"Error connecting to MySQL database: {e}")
-    finally:
-        # Close cursor and connection
-        if 'cursor' in locals():
-            cursor.close()
-        if 'connection' in locals() and connection.open:
-            connection.close()
 
 
 class MyHandler(FileSystemEventHandler):
@@ -163,7 +182,7 @@ def start_observer(directory, log_file):
 
 
 # Directory to monitor
-directory_to_monitor = "C:\lib"
+directory_to_monitor = "my_folder"
 
 # Log file
 log_file = "log.txt"
